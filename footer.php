e7e2a58e7cffc2be576d462eb9565e20d56710b4
HarshithCJ <harshith@chocolatejunction.in>
2018-10-09 16:11:10 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,57 @@
 </div>
+<div class="container-fluid lw_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <ul>
+                        <li><a href="#">Weebly Themes</a></li>
+                        <li><a href="#">Pre-sale FAQS</a></li>
+                        <li><a href="#">Submit a Ticket</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-2">
+                    <ul>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Theme Tweak</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-2">
+                    <ul>
+                        <li><a href="#">Showcase</a></li>
+                        <li><a href="#">widgetkit</a></li>
+                        <li><a href="#">Support</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-2">
+                    <ul>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">affiliates</a></li>
+                        <li><a href="#">Resources</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 lw_social">
+                    <ul>
+                        <li><a href="http://fb.me" target="_BLANK"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" target="_BLANK"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" target="_BLANK"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#" target="_BLANK"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#" target="_BLANK"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    <div class="copyright">
+        <div class="container contents_footer">
+        Copyright &copy; 2018. All rights Reserved @ LoudWhispers.
+        </div>
+    </div>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
