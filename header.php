251602ed09f8ddfa0716ee6f3b3fc058f1e1664f
CharitCJ <chariteques@users.noreply.github.com>
2018-10-09 10:57:22 +0530

    Update header.php

--- header.php
+++ header.php
@@ -4,26 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php wp_head("Loud Whispers") ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">
+    <title><?php bloginfo('title') ?></title>
     <?php wp_head() ?>
 </head>
 <body>
-    <div id="container">
-    <header>
-    <div class="connect_with_us">
-      <!--  <ul>
-            <li>
+<div class="container">
 
-            </li>
-            <li>
-                
-            </li>
-            <li>
-                
-            </li>
-            <li>
-                
-            </li>
-        </ul> -->
+    <?php
+    if ( is_active_sidebar( 'header_contact' ) ) : ?>
+    <div id="header-widget-area" class="chw-widget-area widget-area" role="complementary">
+    <?php dynamic_sidebar( 'header_contact' ); ?>
     </div>
-    </header>
+    <?php endif; ?>
+
+</div>
+<div class="container-fluid">
+
+    <?php
+    if ( is_active_sidebar( 'header_menu' ) ) : ?>
+    <div id="header-widget-area" class="chw-widget-area widget-area" role="complementary">
+    <?php dynamic_sidebar( 'header_menu' ); ?>
+    </div>
+    <?php endif; ?>
+
+</div>
+<div class="container">
+
