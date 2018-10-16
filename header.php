83aca55527e44467b829e1ea0bfb36e5f260b8c8
HarshithCJ <harshith@chocolatejunction.in>
2018-10-16 18:05:11 +0530

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">
     <title><?php bloginfo('title') ?></title>
